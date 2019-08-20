2897c7f584f54cdb0a333a0225a8b4b25480b6b2
Leelsmuth <femianjorin@rocketmail.com>
2019-08-20 15:19:38 +0100

    completed the MVP for closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,6 +3,13 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
+function firstName() {
+  var name = 'Oluwafemi';
+  function displayName() {
+    console.log(`My name is ${name}`);
+  }
+  return displayName;
+}
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
